c853b1c347b11a24c6411eea765e402a1770eac7
Reed James <reedtjames1234@gmail.com>
2019-06-20 16:26:42 -0500

    reducers might be done, working on actions

---
+++ friends/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE,
+export const FETCH_START,
+export const FETCH_SUCCESS,
+export const FETCH_FAILURE
+    
+    
+    
+    
+    
